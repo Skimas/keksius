0b22bf75a05b07c4db27b974544df73180a8760e
Simonas Daniliauskas <dranxman@gmail.com>
2014-08-15 04:10:56 -0700

    Add update, edit method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def new
+		@article = Article.new
 	end
 
 	def index
@@ -16,6 +17,20 @@ class ArticlesController < ApplicationController
 		   redirect_to @article
 		else
 			render 'new'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
 		end
 	end
 
